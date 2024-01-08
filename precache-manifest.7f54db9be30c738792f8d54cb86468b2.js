76e957cae00a4218181fb8df3686475ed99d2ff7
Abhinav Tilwar <abhinavtilwar03@gmail.com>
2024-01-08 15:02:12 +0530

    Updates

---
+++ precache-manifest.7f54db9be30c738792f8d54cb86468b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2a39386d92f31e52adc4df5f3a622a",
+    "url": "/text-converter/index.html"
+  },
+  {
+    "revision": "a96c6ee15fa30979fa72",
+    "url": "/text-converter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da502336e3106fab2254",
+    "url": "/text-converter/static/js/2.f6f516aa.chunk.js"
+  },
+  {
+    "revision": "2ed7cf08f09a4e670eb4015eec120db8",
+    "url": "/text-converter/static/js/2.f6f516aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c7c4f3a900e08bf725",
+    "url": "/text-converter/static/js/3.e2d6f506.chunk.js"
+  },
+  {
+    "revision": "a96c6ee15fa30979fa72",
+    "url": "/text-converter/static/js/main.da438688.chunk.js"
+  },
+  {
+    "revision": "027d7ad23944b02a1fa3",
+    "url": "/text-converter/static/js/runtime-main.87527d4d.js"
+  }
+]);
